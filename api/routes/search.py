def01fa442d8e893a1d6851f7bddbfb8efbbf992
Arik Smith <arik.m.smith@gmail.com>
2023-11-19 15:08:21 -0500

    add logging to search route

--- api/routes/search.py
+++ api/routes/search.py
@@ -1,9 +1,10 @@
+import logging
 from flask import Blueprint, request
 
 from api.services.tmdb_service import TMDBService
 
 search = Blueprint('search', __name__)
-
+logger = logging.getLogger(__name__)
 
 @search.route('/search/movies', methods=['GET'])
 def search_movie():
@@ -11,6 +12,12 @@ def search_movie():
     try:
         movie_service = TMDBService()
         results = movie_service.search(query)
+        return {'results': results}, 200
     except Exception as e:
-        return {'error': e.args[0]}, 500
-    return {'results': results}, 200
+        raise RuntimeError(e.args[0])
+
+
+@search.errorhandler(RuntimeError)
+def handle_service_error(error):
+    logger.error(f'[SERVER ERROR] {error.args[0]}')
+    return {'error': error.args[0]}, 500
